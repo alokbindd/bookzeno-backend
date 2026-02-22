65ed167e5676eda0e61c3e83161d2e22daa30865
Alok Bind <114241460+alokbind01@users.noreply.github.com>
2026-02-22 23:53:24 +0530

    Add URL pattern for RemoveCartItem to enable item removal from the cart

--- carts/urls.py
+++ carts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CartView, AddToCartView
+from .views import CartView, AddToCartView, RemoveCartItem
 
 urlpatterns = [
     path('', view=CartView.as_view(), name='cart'),
     path('add/', view=AddToCartView.as_view(), name='add-to-cart'),
+    path('remove/', view=RemoveCartItem.as_view(), name='remove-cart-item'),
 ]
