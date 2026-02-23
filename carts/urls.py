1c4635de567e45c63d4aad5bb62a39df69ace06b
Alok Bind <114241460+alokbind01@users.noreply.github.com>
2026-02-23 17:09:00 +0530

    Add URL pattern for MergeCartView to enable cart merging functionality in the API

--- carts/urls.py
+++ carts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import CartView, AddToCartView, RemoveCartItem
+from .views import CartView, AddToCartView, RemoveCartItem, MergeCartView
 
 urlpatterns = [
     path('', view=CartView.as_view(), name='cart'),
     path('add/', view=AddToCartView.as_view(), name='add-to-cart'),
     path('remove/', view=RemoveCartItem.as_view(), name='remove-cart-item'),
+    path('merge/', view=MergeCartView.as_view(), name='merge-cart'),
 ]
