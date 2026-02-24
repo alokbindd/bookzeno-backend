7e0aee05698ea512110c19918db3038756fac57e
Alok Bind <114241460+alokbind01@users.noreply.github.com>
2026-02-24 18:01:26 +0530

    Update URL patterns in carts/urls.py to include DeleteCartItemView and a new endpoint for decrementing cart items, improving item management in the cart.

--- carts/urls.py
+++ carts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import CartView, AddToCartView, RemoveCartItemView, MergeCartView
+from .views import CartView, AddToCartView, RemoveCartItemView, MergeCartView, DeleteCartItemView
 
 urlpatterns = [
     path('', view=CartView.as_view(), name='cart'),
     path('add/', view=AddToCartView.as_view(), name='add-to-cart'),
-    path('remove/', view=RemoveCartItemView.as_view(), name='remove-cart-item'),
     path('merge/', view=MergeCartView.as_view(), name='merge-cart'),
+    path('item/<int:book_id>/', DeleteCartItemView.as_view(), name='delete-cart-item'),
+    path('item/<int:book_id>/decrement/', RemoveCartItemView.as_view(), name='remove-cart-item'),
 ]
