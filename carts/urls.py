fa5cb6d23630016c3ad12a0cd518cd634545b533
Alok Bind <114241460+alokbind01@users.noreply.github.com>
2026-02-24 13:44:45 +0530

    Refactor RemoveCartItem to RemoveCartItemView in URL patterns for consistency and clarity.

--- carts/urls.py
+++ carts/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import CartView, AddToCartView, RemoveCartItem, MergeCartView
+from .views import CartView, AddToCartView, RemoveCartItemView, MergeCartView
 
 urlpatterns = [
     path('', view=CartView.as_view(), name='cart'),
     path('add/', view=AddToCartView.as_view(), name='add-to-cart'),
-    path('remove/', view=RemoveCartItem.as_view(), name='remove-cart-item'),
+    path('remove/', view=RemoveCartItemView.as_view(), name='remove-cart-item'),
     path('merge/', view=MergeCartView.as_view(), name='merge-cart'),
 ]
