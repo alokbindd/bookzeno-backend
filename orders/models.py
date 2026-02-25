a1ab06de1516c17c45d63543f68d10a85c45f577
Alok Bind <114241460+alokbind01@users.noreply.github.com>
2026-02-25 17:07:05 +0530

    Refactor Payment model and update Order model to establish a relationship, enhancing payment management. Remove direct payment reference from Order and add foreign key to Payment, along with status choices for better clarity.

--- orders/models.py
+++ orders/models.py
@@ -2,18 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from books.models import Book
 # Create your models here.
-
-class Payment(models.Model):
-    user            = models.ForeignKey(User, on_delete=models.CASCADE)  
-    payment_id      = models.CharField(max_length=225,unique=True)
-    payment_method  = models.CharField(max_length=100)
-    amount_paid     = models.DecimalField(max_digits=10,decimal_places=2)
-    status          = models.CharField(max_length=100)
-    created_at      = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.payment_id
-    
+ 
 class Order(models.Model):
     STATUS_CHOICE = (
         ('pending','Pending'),
@@ -25,7 +14,6 @@ class Order(models.Model):
 
     order_number    = models.CharField(max_length=225, unique=True)
     user            = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
-    payment         = models.ForeignKey(Payment, on_delete=models.SET_NULL, null=True, blank=True)
     first_name      = models.CharField(max_length=50)
     last_name       = models.CharField(max_length=50)
     email           = models.EmailField()
@@ -62,3 +50,21 @@ class OrderProduct(models.Model):
 
     def __str__(self):
         return self.book.title
+
+class Payment(models.Model):
+    STATUS_CHOICES = (
+        ("completed", "Completed"),
+        ("failed", "Failed"),
+        ("pending", "Pending"),
+    )
+
+    user            = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True) 
+    order           = models.ForeignKey(Order, on_delete=models.PROTECT, related_name='payments')
+    payment_id      = models.CharField(max_length=225,unique=True)
+    payment_method  = models.CharField(max_length=100)
+    amount_paid     = models.DecimalField(max_digits=10,decimal_places=2)
+    status          = models.CharField(max_length=20, choices=STATUS_CHOICES)
+    created_at      = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.payment_id
