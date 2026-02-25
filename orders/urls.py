0620ed935920a66a5e065be781dfaf3f69d5e4ad
Alok Bind <114241460+alokbind01@users.noreply.github.com>
2026-02-25 21:41:42 +0530

    Update order URLs to include CreatePaymentView and CapturePaymentView for enhanced payment processing. Add new URL patterns for creating and capturing payments related to orders.

--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import CheckoutView
+from .views import CheckoutView, CreatePaymentView, CapturePayementView
 
 urlpatterns =[
-    path('checkout/', view=CheckoutView.as_view(), name='checkout')
+    path('checkout/', view=CheckoutView.as_view(), name='checkout'),
+    path('<int:order_id>/create-payment/', view=CreatePaymentView.as_view(), name="create-payment"),
+    path('capture-payment/', view=CapturePayementView.as_view(), name="capture-payment"),
+    # path('',),
+    # path('<int:order_id>/'),
 ]
