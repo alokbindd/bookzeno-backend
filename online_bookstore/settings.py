92590f9513510bcf0849cf23255b316af71940c8
Alok Bind <114241460+alokbind01@users.noreply.github.com>
2026-02-26 19:04:13 +0530

    Add CORS configuration to settings.py, allowing requests from localhost:3000 and enabling all origins. Include drf_spectacular for API documentation support.

--- online_bookstore/settings.py
+++ online_bookstore/settings.py
@@ -29,6 +29,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 # Application definition
 
@@ -44,10 +49,13 @@ INSTALLED_APPS = [
     'carts',
     'orders',
     'core',
-    'rest_framework'
+    'rest_framework',
+    'drf_spectacular',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',  
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -137,8 +145,12 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    #pagination
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
+
+    #APIs docs
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
 
 # Paypal config
