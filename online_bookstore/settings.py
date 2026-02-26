db113fb8478f2e3eafa788f68527d3942853d31d
Alok Bind <114241460+alokbind01@users.noreply.github.com>
2026-02-26 18:17:43 +0530

    Add core app to INSTALLED_APPS and configure pagination settings in REST framework

--- online_bookstore/settings.py
+++ online_bookstore/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'books',
     'carts',
     'orders',
+    'core',
     'rest_framework'
 ]
 
@@ -136,6 +137,8 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
 }
 
 # Paypal config
