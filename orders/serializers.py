76469f160497943afe09ffd2726fc66c1692677e
Alok Bind <114241460+alokbind01@users.noreply.github.com>
2026-02-26 14:28:40 +0530

    Add serializers for order history and details, including nested order product information. Enhance CheckoutSerializer with new fields for improved order management.

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from orders.models import Order, OrderProduct
 
 class CheckoutSerializer(serializers.Serializer):
     first_name      = serializers.CharField(max_length=50)
@@ -12,3 +13,39 @@ class CheckoutSerializer(serializers.Serializer):
     pincode         = serializers.CharField(max_length=20)
     country         = serializers.CharField(max_length=20)
     order_note      = serializers.CharField(required=False, allow_blank=True)
+
+class OrderHistorySerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Order
+        fields = ['id','order_number','order_total','tax','grand_total','status','created_at']
+    
+class OrderProductSerializers(serializers.ModelSerializer):
+    book_title = serializers.CharField(source='book.title', read_only=True)
+    cover_image = serializers.ImageField(source='book.cover_image', read_only=True)
+
+    class Meta:
+        model = OrderProduct
+        fields = ['id','book_title','cover_image','quantity', 'book_price']
+
+
+class OrderDetailserializer(serializers.ModelSerializer):
+    items = OrderProductSerializers(many=True, read_only=True)
+    payments = serializers.SerializerMethodField()
+    
+    class Meta:
+        model = Order
+        fields = ['id','order_number','status','full_name','email','phone_number','full_address','state','city','pincode','country','order_note','order_total','tax','grand_total','items','payments']
+
+    def get_payments(self,obj):
+        payment = obj.payments.filter(status='completed').first()
+        if not payment:
+            return None
+        
+        return {
+            "payment_id": payment.payment_id,
+            "payment_method": payment.payment_method,
+            "amount_paid": payment.amount_paid,
+            "status": payment.status,
+            "created_at": payment.created_at,
+        }
