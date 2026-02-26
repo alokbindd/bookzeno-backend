83587f129620c8dff8a9b7adc7ed81f5989942d3
Alok Bind <114241460+alokbind01@users.noreply.github.com>
2026-02-26 14:26:15 +0530

    Add full_address method to Order model for improved address formatting. This method concatenates address_line_1 and address_line_2 for easier access to complete address information.

--- orders/models.py
+++ orders/models.py
@@ -39,6 +39,9 @@ class Order(models.Model):
     
     def full_name(self):
         return f'{self.first_name} {self.last_name}'
+    
+    def full_address(self):
+        return f'{self.address_line_1}, {self.address_line_2}'
 
 class OrderProduct(models.Model):
     order       = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
