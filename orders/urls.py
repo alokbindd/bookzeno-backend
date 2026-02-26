69135c57146576dca6f27f0d9b31c24d40cd2c26
Alok Bind <114241460+alokbind01@users.noreply.github.com>
2026-02-26 14:35:25 +0530

    Update order URLs to include paths for OrderHistoryView and OrderDetailView, enhancing order management and detail retrieval for users.

--- orders/urls.py
+++ orders/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import CheckoutView, CreatePaymentView, CapturePayementView
+from .views import CheckoutView, CreatePaymentView, CapturePayementView, OrderHistoryView, OrderDetailView
 
 urlpatterns =[
+    path('', view=OrderHistoryView.as_view(), name='order-history'),
+    path('<str:order_number>/',view=OrderDetailView.as_view(),name='order-detail'),
     path('checkout/', view=CheckoutView.as_view(), name='checkout'),
-    path('<int:order_id>/create-payment/', view=CreatePaymentView.as_view(), name="create-payment"),
-    path('capture-payment/', view=CapturePayementView.as_view(), name="capture-payment"),
-    # path('',),
-    # path('<int:order_id>/'),
+    path('<int:order_id>/create-payment/', view=CreatePaymentView.as_view(), name='create-payment'),
+    path('capture-payment/', view=CapturePayementView.as_view(), name='capture-payment'),
 ]
