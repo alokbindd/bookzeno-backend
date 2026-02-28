1a3fb54044102f64a89725e538d14cc94caa0367
Alok Bind <114241460+alokbind01@users.noreply.github.com>
2026-02-28 22:53:35 +0530

    Refactor URL patterns in urls.py for improved readability and organization; maintain existing API routes for accounts, books, carts, and orders.

--- online_bookstore/urls.py
+++ online_bookstore/urls.py
@@ -19,20 +19,15 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
-from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView)
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
-
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('api/',include('accounts.urls')),
-    path('api/',include('books.urls')),
-    path('api/',include('carts.urls')),
-    path('api/',include('orders.urls')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/', include('accounts.urls')),
+    path('api/', include('books.urls')),
+    path('api/', include('carts.urls')),
+    path('api/', include('orders.urls')),
     path("schema/", SpectacularAPIView.as_view(), name="schema"),
     path("docs/", SpectacularSwaggerView.as_view(url_name="schema")),
 ]
