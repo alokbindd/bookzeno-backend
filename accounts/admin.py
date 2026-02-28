8dcd2e3ee767edefd8f1a4d7e7ea62601efafa36
Alok Bind <114241460+alokbind01@users.noreply.github.com>
2026-02-28 12:15:13 +0530

    Register custom User admin with enhanced display and filtering options; unregister default User admin.

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from .models import UserProfile
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
+
 
 # Register your models here.
+admin.site.unregister(User)
 admin.site.register(UserProfile)
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active', 'date_joined')
+    list_filter = ('is_staff', 'is_active', 'groups')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    ordering = ('-date_joined',)
