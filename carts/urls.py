9e7e957df6e0cdbb4fa7d2b486f23b5b7e7c38b1
Alok Bind <114241460+alokbind01@users.noreply.github.com>
2026-02-28 12:11:32 +0530

    Update URL patterns for carts and orders to include 'carts/' and 'orders/' prefixes for improved organization and clarity in routing.

--- carts/urls.py
+++ carts/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import CartView, AddToCartView, RemoveCartItemView, MergeCartView, DeleteCartItemView
 
 urlpatterns = [
-    path('', view=CartView.as_view(), name='cart'),
-    path('add/', view=AddToCartView.as_view(), name='add-to-cart'),
-    path('merge/', view=MergeCartView.as_view(), name='merge-cart'),
-    path('item/<int:book_id>/', DeleteCartItemView.as_view(), name='delete-cart-item'),
-    path('item/<int:book_id>/decrement/', RemoveCartItemView.as_view(), name='remove-cart-item'),
+    path('carts/', view=CartView.as_view(), name='cart'),
+    path('carts/add/', view=AddToCartView.as_view(), name='add-to-cart'),
+    path('carts/merge/', view=MergeCartView.as_view(), name='merge-cart'),
+    path('carts/item/<int:book_id>/', DeleteCartItemView.as_view(), name='delete-cart-item'),
+    path('carts/item/<int:book_id>/decrement/', RemoveCartItemView.as_view(), name='remove-cart-item'),
 ]
--- orders/urls.py
+++ orders/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import CheckoutView, CreatePaymentView, CapturePayementView, OrderHistoryView, OrderDetailView
 
 urlpatterns =[
-    path('', view=OrderHistoryView.as_view(), name='order-history'),
-    path('<str:order_number>/',view=OrderDetailView.as_view(),name='order-detail'),
-    path('checkout/', view=CheckoutView.as_view(), name='checkout'),
-    path('<int:order_id>/create-payment/', view=CreatePaymentView.as_view(), name='create-payment'),
-    path('capture-payment/', view=CapturePayementView.as_view(), name='capture-payment'),
+    path('orders/', view=OrderHistoryView.as_view(), name='order-history'),
+    path('orders/checkout/', view=CheckoutView.as_view(), name='checkout'),
+    path('orders/capture-payment/', view=CapturePayementView.as_view(), name='capture-payment'),
+    path('orders/<str:order_number>/',view=OrderDetailView.as_view(),name='order-detail'),
+    path('orders/<int:order_id>/create-payment/', view=CreatePaymentView.as_view(), name='create-payment'),
 ]
