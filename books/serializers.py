eda6b192cb4dae118512cd120d9eb3c7cd471665
Alok Bind <114241460+alokbind01@users.noreply.github.com>
2026-02-28 12:10:42 +0530

    Add ReviewSerializer to handle user reviews and ratings in the API. Update BookSerializer to include reviews, enhancing the book detail representation.

--- books/serializers.py
+++ books/serializers.py
@@ -1,13 +1,30 @@
-from .models import Category, Book
 from rest_framework import serializers
+from books.models import Category, Book
+from books.models import ReviewRating
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ['id','category_name','slug']
 
+class ReviewSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField(read_only=True)
+    rating = serializers.FloatField(min_value=1, max_value=5)
+
+    class Meta:
+        model = ReviewRating
+        fields = [
+            "id",
+            "user",
+            "subject",
+            "review",
+            "rating",
+            "created_at",
+        ]
+
 class BookSerializer(serializers.ModelSerializer):
     category = CategorySerializer(read_only=True)
+    reviews = ReviewSerializer(read_only=True, many=True)
 
     class Meta:
         model = Book
@@ -23,5 +40,6 @@ class BookSerializer(serializers.ModelSerializer):
             'stock',
             'cover_image',
             'publication_date',
+            'reviews',
         ]
 
