e44ddbc46893110e86a0ee3d4bcc77a8dec7bb05
Alok Bind <114241460+alokbind01@users.noreply.github.com>
2026-02-28 12:15:25 +0530

    Update account URL patterns to include 'auth/' prefix for registration and add activation endpoint for account activation.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import RegisterView
+from .views import RegisterView, ActivateAccountView
 
 urlpatterns = [
-    path('register/',view=RegisterView.as_view(), name='register'),
+    path('auth/register/',view=RegisterView.as_view(), name='register'),
+    path("auth/activate/<str:uid>/<str:token>/", ActivateAccountView.as_view(), name="activate-account"),
 ]
