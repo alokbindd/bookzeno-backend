3ba4a043bec0fe703a2182c3b14c7be6ff0d3103
Alok Bind <114241460+alokbind01@users.noreply.github.com>
2026-02-28 12:09:49 +0530

    Enhance URL routing for book reviews and admin details by adding new paths for submitting, listing, and deleting reviews. Organize public and admin routes for better clarity.

--- books/urls.py
+++ books/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
-from .views import BookView,BookDetailView
+from .views import BookView, BookDetailView, BookReviewListView, SubmitReviewView, DeleteReviewView, AdminBookDetailView
 
 urlpatterns = [
-    path('',view=BookView.as_view(),name='book-list'),
-    path('<slug:slug>/',view=BookDetailView.as_view(),name='book-detail')
+    # Public
+    path('books/',view=BookView.as_view(),name='book-list'),
+    path('books/<slug:slug>/',view=BookDetailView.as_view(),name='book-detail'),
+    path("books/<slug:slug>/review/", SubmitReviewView.as_view()),
+    path("books/<slug:slug>/reviews/", BookReviewListView.as_view()),
+    path("books/<slug:slug>/review/delete/", DeleteReviewView.as_view()),
+
+    # Admin
+    path('admin/books/<slug:slug>/',view=AdminBookDetailView.as_view(),name='admin-book-detail'),
 ]
