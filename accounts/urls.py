b8085932789c64cf265125943a61bd4eb7039af3
Alok Bind <114241460+alokbind01@users.noreply.github.com>
2026-02-28 22:53:56 +0530

    Update account URL patterns to use 'accounts/' prefix and add new endpoints for login, change password, and password reset functionalities.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,20 @@
 from django.urls import path
-from .views import RegisterView, ActivateAccountView
+from .views import (
+    RegisterView,
+    ActivateAccountView,
+    LoginView,
+    CustomTokenRefreshView,
+    ChangePasswordView,
+    PasswordResetRequestView,
+    PasswordResetConfirmView,
+)
 
 urlpatterns = [
-    path('auth/register/',view=RegisterView.as_view(), name='register'),
-    path("auth/activate/<str:uid>/<str:token>/", ActivateAccountView.as_view(), name="activate-account"),
+    path('accounts/register/', RegisterView.as_view(), name='register'),
+    path('accounts/activate/<str:uid>/<str:token>/', ActivateAccountView.as_view(), name='activate'),
+    path('accounts/login/', LoginView.as_view(), name='login'),
+    path('accounts/change-password/', ChangePasswordView.as_view(), name='change-password'),
+    path('accounts/password-reset/', PasswordResetRequestView.as_view(), name='password-reset'),
+    path('accounts/password-reset-confirm/<str:uid>/<str:token>/', PasswordResetConfirmView.as_view(), name='password-reset-confirm'),
+    path('accounts/token/refresh/', CustomTokenRefreshView.as_view(), name='token-refresh'),
 ]
