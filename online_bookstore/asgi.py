15278c457003c4e2295bd82dc03b16fddf32a617
Alok Bind <114241460+alokbind01@users.noreply.github.com>
2026-03-01 16:06:35 +0530

    fix: update project references from 'online_bookstore' to 'bookzeno' in settings, URLs, and management files

--- online_bookstore/asgi.py
+++ online_bookstore/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for online_bookstore project.
+ASGI config for bookzeno project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'online_bookstore.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bookzeno.settings')
 
 application = get_asgi_application()
--- online_bookstore/urls.py
+++ online_bookstore/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for online_bookstore project.
+URL configuration for bookzeno project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
