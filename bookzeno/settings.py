2ce8c4031d8d928fc17baa48f67d07b5d0c3ee88
Alok Bind <114241460+alokbind01@users.noreply.github.com>
2026-03-02 21:01:49 +0530

    feat: configure AWS S3 region and storage settings in settings.py

--- bookzeno/settings.py
+++ bookzeno/settings.py
@@ -141,8 +141,11 @@ if os.environ.get("AWS_STORAGE_BUCKET_NAME"):
     AWS_ACCESS_KEY_ID = config("AWS_ACCESS_KEY_ID")
     AWS_SECRET_ACCESS_KEY = config("AWS_SECRET_ACCESS_KEY")
     AWS_STORAGE_BUCKET_NAME = config("AWS_STORAGE_BUCKET_NAME")
-    AWS_S3_REGION_NAME = "ap-south-1"
-
+    AWS_S3_REGION_NAME = config("AWS_S3_REGION_NAME")
+    
+    AWS_DEFAULT_ACL = None
+    AWS_QUERYSTRING_AUTH = False
+    
     AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 
     DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
