6dc18b5fd2a81aab89b25f8381f86250b2341166
Alok Bind <114241460+alokbind01@users.noreply.github.com>
2026-03-02 21:12:58 +0530

    fix: update AWS S3 custom domain to include region name

--- bookzeno/settings.py
+++ bookzeno/settings.py
@@ -146,7 +146,7 @@ if os.environ.get("AWS_STORAGE_BUCKET_NAME"):
     AWS_DEFAULT_ACL = None
     AWS_QUERYSTRING_AUTH = False
     
-    AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+    AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com"
 
     DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
