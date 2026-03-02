eeb65735db49bbe2e672653ccbc384f1f85e4bc1
Alok Bind <114241460+alokbind01@users.noreply.github.com>
2026-03-02 20:17:39 +0530

    feat: integrate AWS S3 storage configuration for media files

--- bookzeno/settings.py
+++ bookzeno/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'drf_spectacular',
     'corsheaders',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -135,6 +136,18 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Media settings
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+if os.environ.get("AWS_STORAGE_BUCKET_NAME"):
+    AWS_ACCESS_KEY_ID = config("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = config("AWS_SECRET_ACCESS_KEY")
+    AWS_STORAGE_BUCKET_NAME = config("AWS_STORAGE_BUCKET_NAME")
+    AWS_S3_REGION_NAME = "ap-south-1"
+
+    AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+
+    DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+
+    MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/"
 
 # REST_FRAMEWORK config
 REST_FRAMEWORK = {
