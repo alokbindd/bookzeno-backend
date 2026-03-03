223d8d12df5eabedf5df9ceef6df562cbf4b12bd
Alok Bind <114241460+alokbind01@users.noreply.github.com>
2026-03-03 21:04:13 +0530

    Restored settings.py from commit id

--- bookzeno/settings.py
+++ bookzeno/settings.py
@@ -51,7 +51,6 @@ INSTALLED_APPS = [
     'rest_framework',
     'drf_spectacular',
     'corsheaders',
-    'storages',
 ]
 
 MIDDLEWARE = [
@@ -134,23 +133,8 @@ STATIC_URL = "/static/"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Media settings
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = BASE_DIR / 'media'
-
-# if os.environ.get("AWS_STORAGE_BUCKET_NAME"):
-AWS_ACCESS_KEY_ID = config("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = config("AWS_SECRET_ACCESS_KEY")
-AWS_STORAGE_BUCKET_NAME = config("AWS_STORAGE_BUCKET_NAME")
-AWS_S3_REGION_NAME = config("AWS_S3_REGION_NAME")
-
-AWS_DEFAULT_ACL = None
-AWS_QUERYSTRING_AUTH = False
-    
-AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
-
-DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-
-MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/"
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # REST_FRAMEWORK config
 REST_FRAMEWORK = {
