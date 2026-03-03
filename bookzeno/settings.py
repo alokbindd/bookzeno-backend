4e8295a80050fda87d547b43591eaf469106fafc
Alok Bind <114241460+alokbind01@users.noreply.github.com>
2026-03-03 22:01:49 +0530

    feat: add AWS S3 media storage configuration with fallback to local filesystem

--- bookzeno/settings.py
+++ bookzeno/settings.py
@@ -133,8 +133,33 @@ STATIC_URL = "/static/"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Media settings
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+AWS_STORAGE_BUCKET_NAME = config("AWS_STORAGE_BUCKET_NAME", default=None)
+
+if AWS_STORAGE_BUCKET_NAME:
+    AWS_ACCESS_KEY_ID = config("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = config("AWS_SECRET_ACCESS_KEY")
+    AWS_S3_REGION_NAME = config("AWS_S3_REGION_NAME")
+
+    AWS_DEFAULT_ACL = None
+    AWS_S3_FILE_OVERWRITE = False
+    # IMPORTANT: Enable signed URLs
+    AWS_QUERYSTRING_AUTH = True
+
+    # Optional: URL expiry time (seconds)
+    AWS_QUERYSTRING_EXPIRE = 3600  # 1 hour
+    STORAGES = {
+        "default": {
+            "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
+        },
+        "staticfiles": {
+            "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        },
+    }
+    MEDIA_URL = f"https://{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com/"
+else:
+    # local filesystem fallback
+    MEDIA_URL = "/media/"
+    MEDIA_ROOT = BASE_DIR / "media"
 
 # REST_FRAMEWORK config
 REST_FRAMEWORK = {
