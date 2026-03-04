3453c6bb6b38fef3213f50d6e28099b7fce9005d
Alok Bind <114241460+alokbind01@users.noreply.github.com>
2026-03-04 18:22:42 +0530

    feat: add URL pattern for CategoryDetailView to retrieve books by category slug

--- category/urls.py
+++ category/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from category.views import CatergoryView
+from category.views import CatergoryView, CatergoryDetailView
 
 urlpatterns = [
     path("category/", CatergoryView.as_view(), name='category-list'),
+    path("category/<slug:slug>/", CatergoryDetailView.as_view(), name='category-books'),
 ]
