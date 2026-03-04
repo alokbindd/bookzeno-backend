e65793836cba30e186fca28c32a6d3ef5e83f886
Alok Bind <114241460+alokbind01@users.noreply.github.com>
2026-03-04 18:22:17 +0530

    feat: add CategoryDetailView to retrieve books by category slug

--- category/views.py
+++ category/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
-from books.models import Category
-from books.serializers import CategorySerializer
+from django.shortcuts import render, get_object_or_404, get_list_or_404
+from books.models import Category, Book
+from books.serializers import CategorySerializer, BookSerializer
 from rest_framework import generics
+from rest_framework.decorators import APIView
 from rest_framework.permissions import AllowAny
+from core.utils import success_response, error_response
 
 # Create your views here.
 
@@ -10,3 +12,10 @@ class CatergoryView(generics.ListAPIView):
     queryset = Category.objects.all().order_by("category_name")
     serializer_class = CategorySerializer
     permission_classes = [AllowAny]
+
+class CatergoryDetailView(APIView):
+    def get(self, request, slug):
+        category = get_object_or_404(Category, slug=slug)
+        book = get_list_or_404(Book, category=category)
+        serializer = BookSerializer(book, many=True)
+        return success_response(data=serializer.data)
