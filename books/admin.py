4f23dd87a36e96efe5be5c0cd9c77cfaa1707440
Alok Bind <114241460+alokbind01@users.noreply.github.com>
2026-03-05 11:08:20 +0530

    feat: add ReviewRatingAdmin to manage review ratings in the admin panel

--- books/admin.py
+++ books/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from books.models import Book, Category
+from books.models import Book, Category, ReviewRating
 
 
 class CategoryAdmin(admin.ModelAdmin):
@@ -11,7 +11,12 @@ class BookAdmin(admin.ModelAdmin):
     list_display = ('id','category','title','slug','isbn','author','price','publication_date','stock')
     ordering = ('-publication_date',)
 
+class ReviewRatingAdmin(admin.ModelAdmin):
+    list_display = ('id','user','book','subject','rating','status')
+    list_editable = ('status',)
+    ordering = ('id',)
 
 # Register your models here.
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Book, BookAdmin)
+admin.site.register(ReviewRating, ReviewRatingAdmin)
