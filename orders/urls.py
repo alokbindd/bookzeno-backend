4985af50b3f54f1b42f046689ef6673e9b2ce049
Alok Bind <114241460+alokbind01@users.noreply.github.com>
2026-03-07 21:33:01 +0530

    update url patterns

--- orders/urls.py
+++ orders/urls.py
@@ -6,7 +6,7 @@ from .views import (CapturePayementView, CheckoutView, CreatePaymentView,
 urlpatterns =[
     path('orders/', view=OrderHistoryView.as_view(), name='order-history'),
     path('orders/checkout/', view=CheckoutView.as_view(), name='checkout'),
-    path('orders/capture-payment/', view=CapturePayementView.as_view(), name='capture-payment'),
+    path('orders/<str:order_number>/payment/create/', view=CreatePaymentView.as_view(), name='create-payment'),
+    path('orders/payment/capture/', view=CapturePayementView.as_view(), name='capture-payment'),
     path('orders/<str:order_number>/',view=OrderDetailView.as_view(),name='order-detail'),
-    path('orders/<int:order_id>/create-payment/', view=CreatePaymentView.as_view(), name='create-payment'),
 ]
