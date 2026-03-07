bf4ff4801b0c989046b0691ecbf42f08f7adb96b
Alok Bind <114241460+alokbind01@users.noreply.github.com>
2026-03-07 19:37:50 +0530

    feat: add book_slug field to CartItemSerializer for improved cart item details

--- carts/serializers.py
+++ carts/serializers.py
@@ -8,8 +8,9 @@ class CartItemSerializer(serializers.ModelSerializer):
     book_title = serializers.ReadOnlyField(source='book.title')
     book_price = serializers.ReadOnlyField(source='book.price')
     book_image = serializers.ImageField(source='book.cover_image', read_only=True)
+    book_slug = serializers.CharField(source='book.slug', read_only=True)
 
     class Meta:
         model = CartItem
-        fields = ['id','book','book_title','book_image','book_price','quantity']
+        fields = ['id','book','book_title','book_image','book_slug','book_price','quantity']
 
