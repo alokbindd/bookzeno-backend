7663dc73d5857fe5903f3eb259c16913c350642f
Alok Bind <114241460+alokbind01@users.noreply.github.com>
2026-03-08 20:55:36 +0530

    feat: add search functionality for books by introducing BookSearchView in URL patterns

--- books/urls.py
+++ books/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
 from .views import (AdminBookDetailView, BookDetailView, BookReviewListView,
-                    BookView, DeleteReviewView, SubmitReviewView)
+                    BookView, DeleteReviewView, SubmitReviewView, BookSearchView)
 
 urlpatterns = [
     # Public
     path('books/',view=BookView.as_view(),name='book-list'),
+    path("books/search/", BookSearchView.as_view(), name="book-search"),
     path('books/<slug:slug>/',view=BookDetailView.as_view(),name='book-detail'),
     path("books/<slug:slug>/review/", SubmitReviewView.as_view()),
     path("books/<slug:slug>/reviews/", BookReviewListView.as_view()),
