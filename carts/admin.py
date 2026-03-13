91b0596afe99958abd7c56ccd4b05342a322a60f
Alok Bind <114241460+alokbind01@users.noreply.github.com>
2026-03-13 14:00:59 +0530

    feat: enhance CartAdmin by adding CartItem inline display for better management of cart items

--- carts/admin.py
+++ carts/admin.py
@@ -2,6 +2,14 @@ from django.contrib import admin
 
 from .models import Cart, CartItem
 
+class CartItemInline(admin.TabularInline):
+    model = CartItem
+    readonly_fields = ['book','quantity']
+    extra = 0
+
+class CartAdmin(admin.ModelAdmin):
+    inlines = [CartItemInline]
+
 # Register your models here.
-admin.site.register(Cart)
+admin.site.register(Cart, CartAdmin)
 admin.site.register(CartItem)
