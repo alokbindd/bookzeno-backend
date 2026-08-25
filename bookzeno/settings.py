3a7b6af85c8237240f6bd8676f0bb008081f17ed
Alok Bind <114241460+alokbind01@users.noreply.github.com>
2026-08-25 14:46:12 +0530

    refactor: comment out SMTP email configuration in settings.py

--- bookzeno/settings.py
+++ bookzeno/settings.py
@@ -202,9 +202,9 @@ PAYPAL_SECRET_KEY=config("PAYPAL_SECRET_KEY")
 FRONTEND_URL = config('FRONTEND_URL', default='')
 
 # smtp configuration
-EMAIL_HOST = config('EMAIL_HOST', default='')
-EMAIL_PORT = config('EMAIL_PORT',cast=int, default=587)
-EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
-EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+# EMAIL_HOST = config('EMAIL_HOST', default='')
+# EMAIL_PORT = config('EMAIL_PORT',cast=int, default=587)
+# EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
+# EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
+# EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
+# DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
